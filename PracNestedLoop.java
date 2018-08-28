5e0fa0884c2c571cc26528f82f9c6ff8501bc22e
TAREQ HASAN <akmhasan18@gmail.com>
2018-08-27 20:16:39 -0400

    our practice

---
+++ PracNestedLoop.java
@@ -0,0 +1,15 @@
+package Practice082718;
+
+public class PracNestedLoop {
+    public static void main(String[] args) {
+        //int row = 6;
+        for (int row = 1; row <= 6; row++){
+            for (int column= 1; column <=6; column++){
+                System.out.println(column * row);
+
+            }
+            System.out.println("\t");
+        }
+
+    }
+}
