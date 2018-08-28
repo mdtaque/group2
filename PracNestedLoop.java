947740ad1da50b0fa00f962a8c0d0a1c1e0ecbb2
mdtaque <41880907+mdtaque@users.noreply.github.com>
2018-08-27 22:41:20 -0400

    Update PracNestedLoop.java

--- PracNestedLoop.java
+++ PracNestedLoop.java
@@ -6,6 +6,7 @@ public class PracNestedLoop {
         for (int row = 1; row <= 6; row++){
             for (int column= 1; column <=6; column++){
                 System.out.println(column * row);
+                System.out.println("checked in 11:30);
 
             }
             System.out.println("\t");
